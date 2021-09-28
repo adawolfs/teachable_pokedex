deb76ac561e23eca78bc33c310c81f8a93320cc5
adawolfs <adawolfs@gmail.com>
2021-09-28 19:46:15 +0000

    Deploying to gh-pages from @ adawolfs/teachable_pokedex@3d5ac9ab7cbc5d16f396809dea1b60863ab3f780 🚀

---
+++ assets/js/pokedex_ml.js
@@ -0,0 +1,52 @@
+// More API functions here:
+// https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
+
+// the link to your model provided by Teachable Machine export panel
+const URL = "assets/my_model/";
+var runningCamera = false;
+
+let model, webcam, labelContainer, maxPredictions;
+
+// Load the image model and setup the webcam
+async function init(containerId) {
+    if (runningCamera) {
+        return
+    }
+    const modelURL = URL + "model.json";
+    const metadataURL = URL + "metadata.json";
+    const container = document.getElementById(containerId)
+    // load the model and metadata
+    // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+    // or files from your local hard drive
+    // Note: the pose library adds "tmImage" object to your window (window.tmImage)
+    model = await tmImage.load(modelURL, metadataURL);
+    maxPredictions = model.getTotalClasses();
+
+    // Convenience function to setup a webcam
+    const flip = false; // whether to flip the webcam
+    webcam = new tmImage.Webcam(container.offsetWidth, container.offsetHeight - 10, flip); // width, height, flip
+    await webcam.setup(); // request access to the webcam
+    await webcam.play();
+    window.requestAnimationFrame(loop);
+
+    // append elements to the DOM
+    container.appendChild(webcam.canvas);
+    //container.removeChild(container.childNodes[0]);
+    runningCamera = true;
+}
+
+async function loop() {
+    webcam.update(); // update the webcam frame
+    await predict();
+    window.requestAnimationFrame(loop);
+}
+
+// run the webcam image through the image model
+async function predict() {
+    // predict can take in an image, video or canvas html element
+    const prediction = await model.predict(webcam.canvas);
+    for (let i = 0; i < maxPredictions; i++) {
+        const classPrediction =
+            prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+    }
+}
---
+++ assets/js/pokedex_ui.js
@@ -0,0 +1,64 @@
+(function() {
+
+    navigator.mediaDevices.enumerateDevices().then((devices) =>{
+        console.log(devices);
+    });
+    const _lights_canvas = document.getElementById('lights');
+    const _base_canvas = document.getElementById('base');
+    const _controls_canvas = document.getElementById('controls');
+    const _screen_canvas = document.getElementById('screen');
+    const _screen_ctx = _screen_canvas.getContext('2d');
+    const _screen_container = document.getElementsByClassName('screen')[0];
+    // resize the canvas to fill browser window dynamically
+    window.addEventListener('resize', resizeCanvas, false);
+            
+    function resizeCanvas() {
+        _lights_canvas.width = 1000;
+        _lights_canvas.height = 1500;
+        _base_canvas.width = 1000;
+        _base_canvas.height = 1500;
+
+        _controls_canvas.width = 1000;
+        _controls_canvas.height = 500;
+        _screen_canvas.width = 1000;
+        _screen_canvas.height = 1000;
+                    
+        /**
+         * Your drawings need to be inside this function otherwise they will be reset when 
+         * you resize the browser window and the canvas goes will be cleared.
+         */
+        drawStuff(); 
+    }
+    
+    resizeCanvas();
+            
+    function drawStuff() {
+        const rive_pokedex_ui = 'assets/rive/pokedex.riv';
+        const lights = new rive.Rive({
+            src: rive_pokedex_ui,
+            canvas: document.getElementById('lights'),
+            artboard: 'lights',
+            autoplay: false,
+        });
+        const base = new rive.Rive({
+            src: rive_pokedex_ui,
+            canvas: document.getElementById('base'),
+            artboard: 'base',
+            autoplay: false,
+        });
+        const screen = new rive.Rive({
+            src: rive_pokedex_ui,
+            canvas: document.getElementById('screen'),
+            artboard: 'screen',
+            autoplay: false,
+        });
+        const controls = new rive.Rive({
+            src: rive_pokedex_ui,
+            canvas: document.getElementById('controls'),
+            artboard: 'controls',
+            autoplay: false,
+        });
+        lights.layout = new rive.Layout({fit: rive.Fit.FitWidth, alignment: rive.Alignment.TopCenter});
+        base.layout = new rive.Layout({fit: rive.Fit.FitWidth, alignment: rive.Alignment.BottomCenter});
+    }
+})();
